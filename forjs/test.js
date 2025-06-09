bacca5272af05c175cff54dbd13b25c311b5c158
Anurag Dwivedi <anurag1710dwivedi@gmail.com>
2025-06-09 11:20:01 +0000

    learn datatypes

--- forjs/test.js
+++ forjs/test.js
@@ -1,1 +1,10 @@
-console.log("anurag")
+const acountId=12345
+let acountEmail="anurag@gmail.com"
+var accountPassword="qwerty"
+accountCity="allahabad"
+let accountstate;
+// acountId=2
+acountEmail="anuragd@gmail.com"
+accountPassword="power"
+console.log(acountEmail);
+console.table([acountId,acountEmail,accountPassword,accountCity,accountstate])
